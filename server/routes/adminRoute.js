5c0a8803a40c6216f892d7fb0ce053f0acec1704
Abdul-Git331 <ak1688535@gmail.com>
2025-09-25 19:06:21 +0530

    Update adminRoute.js

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -96,8 +96,8 @@ adminRoute.post("/admin-login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       path: "/",
       maxAge: 24 * 60 * 60 * 1000,
     });
