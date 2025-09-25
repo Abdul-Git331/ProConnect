ae79164aa2afa4a5a2d5be5284410b343659c8d9
Abdul-Git331 <ak1688535@gmail.com>
2025-09-25 19:44:24 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const adminRoute = require("./routes/adminRoute");
 const app = express();
 
 app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views")); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
